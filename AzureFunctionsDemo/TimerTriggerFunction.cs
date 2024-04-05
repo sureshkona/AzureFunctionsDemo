3cb0af1337fe214aab654379d67e08a8730c3efe
kona suresh <Suresh_Kona@Swissre.com>
2024-04-05 20:40:31 +0530

    Timer Trigger Created

---
+++ AzureFunctionsDemo/TimerTriggerFunction.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+
+namespace AzureFunctionsDemo
+{
+    public class TimerTriggerFunction
+    {
+        private readonly ILogger _logger;
+
+        public TimerTriggerFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TimerTriggerFunction>();
+        }
+
+        [Function("TimerTriggerFunction")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+           
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
